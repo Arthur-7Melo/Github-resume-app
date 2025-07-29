6fff0dbc85596b865369b417cfdc07a4cabb9171
Arthur Melo <arthur7m@gmail.com>
2025-07-29 11:05:30 -0300

    chore(insights): Cria estrutura inicial do insights-service com express

---
+++ insights-service/src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { PORT } from "./config";
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Insights Service rodando na porta: ${PORT}`)
+})
